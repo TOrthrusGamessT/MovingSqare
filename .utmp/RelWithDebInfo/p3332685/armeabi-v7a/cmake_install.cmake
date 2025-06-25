e5c1b4192951a9a8fe2f2958de30c82f4109e1a2
VisnevschiV <visnevskivlad@gmail.com>
2025-06-25 20:29:10 +0200

    some build fixes

--- .utmp/RelWithDebInfo/p3332685/armeabi-v7a/cmake_install.cmake
+++ .utmp/RelWithDebInfo/p3332685/armeabi-v7a/cmake_install.cmake
@@ -44,7 +44,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/unityProj/MovingSqare/.utmp/RelWithDebInfo/p3332685/armeabi-v7a/GameActivity/cmake_install.cmake")
   include("D:/unityProj/MovingSqare/.utmp/RelWithDebInfo/p3332685/armeabi-v7a/FramePacing/cmake_install.cmake")
 
 endif()
