c3b32129934cc525257bafa600b40146e141bbad
VisnevschiV <visnevskivlad@gmail.com>
2025-07-11 16:50:43 +0200

    tutorial animation and some fixing

--- Assets/_ProjectAssets/Scripts/UI/MenuAnimation.cs
+++ Assets/_ProjectAssets/Scripts/UI/MenuAnimation.cs
@@ -7,19 +7,20 @@ using UnityEngine.UI;
 
 public class MenuAnimation : MonoBehaviour
 {
-    public TextMeshProUGUI[] textFade;
     public TextMeshProUGUI money;
-    public GameObject main, shop, topBar, startButton, gameModeMenu;
+    public GameObject  shopButton, lvlButton;
     public SceneLoader SceneLoader;
-    public GameObject lvlMenu;
+    public GameObject lvlMenu, shopMenu;
+
+    private Coroutine shopButtonAnimationCoroutine;
 
     private void Start()
     {
         InitPlayerPrefs();
-        InitAnimations();
+        CheckAndAnimateShopButton();
     }
 
-
+    //TODO: I dont like it here, needs to be replaced
     private void InitPlayerPrefs()
     {
         money.text = PlayerPrefs.HasKey("Money") ? PlayerPrefs.GetInt("Money").ToString() : "0";
@@ -29,24 +30,68 @@ public class MenuAnimation : MonoBehaviour
         }
     }
 
-    private void InitAnimations()
+    private void CheckAndAnimateShopButton()
     {
+        int currentMoney = PlayerPrefs.GetInt("Money", 0);
+        bool tutorialPassed = PlayerPrefs.GetInt("TutorialPassed", 0) == 1;
 
-        for (int i = 0; i < textFade.Length; i++)
+        if (currentMoney > 0 && !tutorialPassed)
         {
-            textFade[i].color = new Color32(255, 255, 255, 1);
-            int index = i;
-            LeanTween.value(0, 1, 1f).setOnUpdate(value =>
-            {
-                Color c = textFade[index].color;
-                c.a = value;
-                textFade[index].color = c;
-            });
+            StartShopButtonAnimation();
         }
-
     }
 
+    private void StartShopButtonAnimation()
+    {
+        if (shopButtonAnimationCoroutine != null)
+        {
+            StopCoroutine(shopButtonAnimationCoroutine);
+        }
+        shopButtonAnimationCoroutine = StartCoroutine(AnimateShopButton());
+    }
 
+    private void StopShopButtonAnimation()
+    {
+        if (shopButtonAnimationCoroutine != null)
+        {
+            StopCoroutine(shopButtonAnimationCoroutine);
+            shopButtonAnimationCoroutine = null;
+            shopButton.transform.localScale = Vector3.one; // Reset scale
+        }
+    }
+
+    private IEnumerator AnimateShopButton()
+    {
+        Vector3 originalScale = shopButton.transform.localScale;
+        Vector3 targetScale = originalScale * 1.2f;
+
+        while (true)
+        {
+            // Scale up
+            float elapsedTime = 0f;
+            float duration = 0.5f;
+            
+            while (elapsedTime < duration)
+            {
+                shopButton.transform.localScale = Vector3.Lerp(originalScale, targetScale, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            // Scale down
+            elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                shopButton.transform.localScale = Vector3.Lerp(targetScale, originalScale, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.5f); // Pause between animations
+        }
+    }
+
+    //used on Level button
     public void ShowLvlMenu()
     {
         UniTask.Void(async () =>
@@ -63,7 +108,7 @@ public class MenuAnimation : MonoBehaviour
 
         });
     }
-
+    // Used on Level menu close button
     public void HideLvlMenu()
     {
         UniTask.Void(async () =>
@@ -75,15 +120,20 @@ public class MenuAnimation : MonoBehaviour
             lvlMenu.SetActive(false);
         });
         FireBase.LogCustomEvent("lvl_menu_closed", new System.Collections.Generic.Dictionary<string, object>
-        {
-
-        });
-
-
+        {});
     }
 
+    // Used on Shop button
     public void GoShop()
     {
+        // Mark tutorial as passed when shop is opened
+        if (PlayerPrefs.GetInt("TutorialPassed", 0) == 0)
+        {
+            PlayerPrefs.SetInt("TutorialPassed", 1);
+            PlayerPrefs.Save();
+            StopShopButtonAnimation();
+        }
+
         SceneLoader.RandomTransition();
         //StartCoroutine(AnimateTransition(1, 30,0));
         StartCoroutine(ShowHideShop(true, 1f));
@@ -93,6 +143,7 @@ public class MenuAnimation : MonoBehaviour
             // Add any parameters you want to log with this event
         });
     }
+
     public void LeaveShop()
     {
         SceneLoader.RandomTransition();
@@ -103,53 +154,10 @@ public class MenuAnimation : MonoBehaviour
             // Add any parameters you want to log with this event
         });
     }
-    IEnumerator AnimateTransition(float from, float to, float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        if (waitTime > 0)
-        {
-            shop.SetActive(false);
-        }
-        LeanTween.value(from, to, 0.5f).setOnUpdate(value =>
-        {
-            Vector3 temp = new Vector3(value, value, 1);
-            main.transform.localScale = temp;
-            //topBar.transform.localScale = temp;
-        });
-
-    }
     IEnumerator ShowHideShop(bool state, float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
 
-        shop.SetActive(state);
-    }
-
-    public void ChoseGameMode()
-    {
-        LeanTween.rotate(startButton, new Vector3(0, 0, 360), 2f);
-        StartCoroutine(ShowGameModeMenu());
-
-    }
-    public IEnumerator ShowGameModeMenu()
-    {
-        yield return new WaitForSeconds(0.1f);
-        gameModeMenu.GetComponent<HorizontalLayoutGroup>().spacing = -650f;
-        gameModeMenu.SetActive(true);
-        startButton.SetActive(false);
-        foreach (Transform child in gameModeMenu.transform)
-        {
-            //LeanTween.rotate(child.gameObject, new Vector3(0, 0, 360), 2f);
-            LeanTween.rotateLocal(child.gameObject, new Vector3(0, 0, 360), 0.7f);
-        }
-        LeanTween.value(-650f, 20f, 0.7f).setOnUpdate(value =>
-        {
-            gameModeMenu.GetComponent<HorizontalLayoutGroup>().spacing = value;
-        });
-
-        FireBase.LogCustomEvent("game_mode_menu_opened", new System.Collections.Generic.Dictionary<string, object>
-        {
-            // Add any parameters you want to log with this event
-        });
+        shopMenu.SetActive(state);
     }
 }
