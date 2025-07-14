c83f38a715110146d3de9432da224dd45a4e81df
leggacys <bogatu.daniel21@gmail.com>
2025-07-14 03:27:56 +0200

    Modify BuildScript

--- Assets/_ProjectAssets/Editor/BuildScript.cs
+++ Assets/_ProjectAssets/Editor/BuildScript.cs
@@ -1,34 +1,28 @@
 using System;
+using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 public static class BuildScript
 {
-    static string[] SCENES = { "_ProjectAssets/Scenes/MainMenu.unity",
-                               "_ProjectAssets/Scenes/Survive.unity",
-                               "_ProjectAssets/Scenes/LvlsScene.unity",};
+    static readonly string[] SCENES = {
+        "_ProjectAssets/Scenes/MainMenu.unity",
+        "_ProjectAssets/Scenes/Survive.unity",
+        "_ProjectAssets/Scenes/LvlsScene.unity",
+    };
 
     public static void PerformBuildAndroid()
     {
+        string buildNumber = Environment.GetEnvironmentVariable("BUILD_NUMBER") ?? "local";
+
         string buildDirectory = "Builds/Android";
-        if (!System.IO.Directory.Exists(buildDirectory))
-        {
-            System.IO.Directory.CreateDirectory(buildDirectory);
-        }
+        Directory.CreateDirectory(buildDirectory);
 
+        string pathToBuild = Path.Combine(buildDirectory, $"v1.6.{buildNumber}.apk");
 
-        string buildNumber = Environment.GetEnvironmentVariable("BUILD_NUMBER");
-        if (string.IsNullOrEmpty(buildNumber))
-        {
-            buildNumber = "local";
-        }
-
-        string pathToBuild = $"Builds/Android/v1.6.{buildNumber}.apk";
-
-        PlayerSettings.Android.keystoreName = System.Environment.GetEnvironmentVariable("KEYSTORE_PATH");
-        PlayerSettings.Android.keystorePass = System.Environment.GetEnvironmentVariable("KEYSTORE_PASS");
-        PlayerSettings.Android.keyaliasName = System.Environment.GetEnvironmentVariable("KEY_ALIAS");
-        PlayerSettings.Android.keyaliasPass = System.Environment.GetEnvironmentVariable("KEY_ALIAS_PASS");
+        PlayerSettings.Android.keystoreName = Environment.GetEnvironmentVariable("KEYSTORE_PATH");
+        PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("KEYSTORE_PASS");
+        PlayerSettings.Android.keyaliasName = Environment.GetEnvironmentVariable("KEY_ALIAS");
+        PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("KEY_ALIAS_PASS");
 
         BuildPipeline.BuildPlayer(SCENES, pathToBuild, BuildTarget.Android, BuildOptions.None);
     }
